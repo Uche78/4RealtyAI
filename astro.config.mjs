da5da33510e6799309b7c20532651983e5f4e4fe
Uche78 <prdtools78@gmail.com>
2025-10-22 00:39:06 -0400

    Update astro.config.mjs
    Added Netlify info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server', // Important! This enables API routes
+  adapter: netlify()
   integrations: [tailwind()],
   site: 'https://4realtyai.com',
   compressHTML: true,
