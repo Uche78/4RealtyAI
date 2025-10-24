5ce5d57628471df47acb8169033c5430e7f6f015
Uche78 <prdtools78@gmail.com>
2025-10-24 02:45:43 -0400

    Update astro.config.mjs
    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
